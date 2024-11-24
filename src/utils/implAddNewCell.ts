c3690c8f5f22731cf1984aa13e948fe76b983480
goranikin <goranikin@snu.ac.kr>
2024-11-25 00:17:34 +0900

    implAddNewCell.ts 생성

---
+++ src/utils/implAddNewCell.ts
@@ -0,0 +1,23 @@
+type gridType = number[][]
+
+export const implAddNewCell = {
+  addNewCell: (grid: gridType): gridType => {
+    const newGrid = grid.map((row) => [...row]);
+
+    const randomZeroPosition = newGrid
+      .flatMap((row, i) =>
+        row.flatMap((cell, j) => (cell !== 0 ? [] : [{ i, j }])),
+      )
+      .sort(() => Math.random() - 0.5)[0];
+    
+    if (randomZeroPosition === undefined) return grid;
+
+    return newGrid.map((row, i) => {
+      return row.map((cell, j) => {
+        return i === randomZeroPosition.i && j === randomZeroPosition.j
+        ? Math.random() > 0.2 ? 2 : 4
+          : cell;
+      });
+    });
+  },
+};
